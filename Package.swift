052269829d35fb9f536f3740169068ff99e71cc5
Pratik Patel <pratik.patel@perplexity.ai>
2025-09-15 10:34:11 -0700

    Explicitly set linked framework

--- Package.swift
+++ Package.swift
@@ -2,24 +2,42 @@
 import PackageDescription
 
 let package = Package(
-    name: "Nuke",
-    platforms: [
-        .iOS(.v13),
-        .tvOS(.v13),
-        .macOS(.v10_15),
-        .watchOS(.v6),
-        .visionOS(.v1),
-    ],
-    products: [
-        .library(name: "Nuke", targets: ["Nuke"]),
-        .library(name: "NukeUI", targets: ["NukeUI"]),
-        .library(name: "NukeVideo", targets: ["NukeVideo"]),
-        .library(name: "NukeExtensions", targets: ["NukeExtensions"])
-    ],
-    targets: [
-        .target(name: "Nuke"),
-        .target(name: "NukeUI", dependencies: ["Nuke"]),
-        .target(name: "NukeVideo", dependencies: ["Nuke"]),
-        .target(name: "NukeExtensions", dependencies: ["Nuke"])
-    ]
+  name: "Nuke",
+  platforms: [
+    .iOS(.v13),
+    .tvOS(.v13),
+    .macOS(.v10_15),
+    .watchOS(.v6),
+    .visionOS(.v1),
+  ],
+  products: [
+    .library(name: "Nuke", targets: ["Nuke"]),
+    .library(name: "NukeUI", targets: ["NukeUI"]),
+    .library(name: "NukeVideo", targets: ["NukeVideo"]),
+    .library(name: "NukeExtensions", targets: ["NukeExtensions"]),
+  ],
+  targets: [
+    .target(name: "Nuke"),
+    .target(
+      name: "NukeUI",
+      dependencies: ["Nuke"],
+      linkerSettings: [
+        .linkedFramework("Nuke", .when(platforms: [.iOS])),
+      ],
+    ),
+    .target(
+      name: "NukeVideo",
+      dependencies: ["Nuke"],
+      linkerSettings: [
+        .linkedFramework("Nuke", .when(platforms: [.iOS])),
+      ],
+    ),
+    .target(
+      name: "NukeExtensions",
+      dependencies: ["Nuke"],
+      linkerSettings: [
+        .linkedFramework("Nuke", .when(platforms: [.iOS])),
+      ],
+    ),
+  ]
 )
